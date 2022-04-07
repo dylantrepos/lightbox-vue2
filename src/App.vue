439683049f5eb90b5116be095d9e3fc168510b80
Dylan Trepos <dylan.trepos@gmail.com>
2022-04-07 15:47:25 +0200

    First boilerplate with the basic Webpack template

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <a href="http://www.picsum.photos/id/1003/800/600" v-lightbox>
+      <img src="http://www.picsum.photos/id/1003/150/150" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
